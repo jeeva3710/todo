7fe978cec5afeeb6bacb63bcfa509e91d1908b65
unknown <jeeva3710@gmail.com>
2025-06-10 11:53:39 +0530

    Prepared for Render deployment

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-7=%=wwkpddcec1oh1(g5ug^6r!+lxfuws)23m9v=2!audixe5o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
